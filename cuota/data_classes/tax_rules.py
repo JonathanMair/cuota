7d146dce42f48aad217bd648a392c72c11fbf2c0
Jonathan Mair <jonathan.mair@gmail.com>
2024-12-05 21:11:05 +0100

    added docstrings

--- cuota/data_classes/tax_rules.py
+++ cuota/data_classes/tax_rules.py
@@ -5,6 +5,7 @@ import numpy as np
 
 from cuota.data_classes.interfaces import AllowanceFunction
 
+
 class Band(BaseModel):
     """
     Represents a tax band with a defined range and payment structure.
@@ -12,20 +13,26 @@ class Band(BaseModel):
     Attributes:
         floor (int): The lower limit of the band. Defaults to 0.
         ceiling (int): The upper limit of the band. Defaults to 200,000.
-        rate (float | None): The percentage rate to apply within the band. Must be between 0 and 1.
-        flat_charge (int | None): A fixed charge to apply within the band. Mutually exclusive with `rate`.
-        exclusive (bool): Indicates whether only the single relevant band applies. Defaults to False.
+        rate (float | None): The percentage rate applied to amounts within the band. Must be between 0 and 1.
+        flat_charge (int | None): A fixed charge applied to the band. Mutually exclusive with `rate`.
+        exclusive (bool): If True, only the single relevant band applies. Defaults to False.
 
     Methods:
-        check_floor_ceiling: Validates the integrity of the band's attributes.
-        get_payable: Calculates the payable amount based on an input value.
+        check_floor_ceiling() -> Self:
+            Validates the integrity of the band's attributes, such as logical consistency and exclusive constraints.
+
+        get_payable(amount: int) -> int:
+            Calculates the payable amount for a given input value within the band.
+
+        convert(rate: float):
+            Adjusts the band's floor, ceiling, and flat charge by a given rate.
     """
 
     floor: int = 0
     ceiling: int = 200000
     rate: float | None = None
     flat_charge: int | None = None
-    exclusive: bool = False  # if True, only payable from the single relevant band
+    exclusive: bool = False
 
     @model_validator(mode="after")
     def check_floor_ceiling(self) -> Self:
@@ -33,82 +40,81 @@ class Band(BaseModel):
         Validates the band's attributes for logical consistency.
 
         Ensures:
-        - Floor and ceiling are not None.
-        - Ceiling is greater than the floor.
-        - Only one of `rate` or `flat_charge` is set.
-        - Rate (if set) is between 0 and 1.
+        - `floor` and `ceiling` are not None.
+        - `ceiling` is greater than `floor`.
+        - Only one of `rate` or `flat_charge` is defined.
+        - `rate` (if defined) is between 0 and 1.
 
         Raises:
-            ValueError: If any validation fails.
+            ValueError: If any of these validations fail.
 
         Returns:
-            Self: The validated Band instance.
+            Self: The validated `Band` instance.
         """
         error_msg = []
-        if self.floor is None:
-            error_msg.append("Floor must not be None.")
-        if self.ceiling is None:
-            error_msg.append("Ceiling must not be None.")
         if self.ceiling <= self.floor:
             error_msg.append("Ceiling must be greater than floor.")
-        if len(error_msg) > 0:
-            error_msg.append("Floor must be smaller than ceiling")
         if self.rate is not None and self.flat_charge is not None:
-            error_msg.append("Only flat_charge or rate must be set, not both.")
+            error_msg.append("Only one of flat_charge or rate must be set, not both.")
         if self.rate is None and self.flat_charge is None:
             error_msg.append("Either flat_charge or rate must be set.")
-        if self.rate is not None and (self.rate > 1 or self.rate < 0):
-            error_msg.append("Rate must be greater than or equal to 0 or less than or equal to 1.")
-        if len(error_msg) > 0:
-            error_msg_joined = "\n" + "\n".join(error_msg)
-            raise ValueError(f"{error_msg_joined}")
+        if self.rate is not None and not (0 <= self.rate <= 1):
+            error_msg.append("Rate must be between 0 and 1.")
+        if error_msg:
+            raise ValueError("\n".join(error_msg))
         return self
 
     def get_payable(self, amount: int) -> int:
         """
-        Calculates the payable amount based on the provided amount.
+        Calculates the payable amount based on the provided value.
 
         Args:
             amount (int): The amount to calculate the payable value for.
 
         Returns:
-            int: The payable amount.
+            int: The payable amount, considering the band's rate or flat charge.
         """
         if self.rate is None:
-            if self.ceiling >= amount > self.floor:
-                return self.flat_charge
-            else:
-                return 0
+            return self.flat_charge if self.ceiling >= amount > self.floor else 0
         if self.exclusive:
-            if self.ceiling >= amount > self.floor:
-                return int(self.rate * amount)
-            else:
-                return 0
-        else:  # this is a non-exclusive % band
-            if amount < self.floor:  # amount is below this band, nothing due
-                return 0
-            elif amount > self.ceiling:  # amount is above ceiling, apply % to whole band
-                return int((self.ceiling - self.floor) * self.rate)
-            else:  # amount falls within the band, apply rate proportionally
-                return int((amount - self.floor) * self.rate)
+            return int(self.rate * amount) if self.ceiling >= amount > self.floor else 0
+        if amount < self.floor:
+            return 0
+        if amount > self.ceiling:
+            return int((self.ceiling - self.floor) * self.rate)
+        return int((amount - self.floor) * self.rate)
 
-    def convert(self, rate):
+    def convert(self, rate: float):
+        """
+        Adjusts the band's floor, ceiling, and flat charge by a given rate.
+
+        Args:
+            rate (float): The conversion rate to scale the band's attributes.
+        """
         self.floor = int(self.floor * rate)
         self.ceiling = int(self.ceiling * rate)
-        self.flat_charge = (self.flat_charge * rate) if self.flat_charge is not None else None
+        if self.flat_charge is not None:
+            self.flat_charge = int(self.flat_charge * rate)
+
 
 class BandsGroup(BaseModel):
     """
     Represents a group of tax bands with an optional allowance.
 
     Attributes:
-        bands (List[Band]): A list of Band instances defining the group.
-        allowance (int): An amount to deduct before applying the bands. Defaults to 0.
+        bands (List[Band]): A list of `Band` instances defining the group.
+        allowance (int | AllowanceFunction): A deduction applied before evaluating the bands.
         name (str | None): An optional name for the group.
 
     Methods:
-        check_bands: Validates the integrity of the band's relationships.
-        get_payable: Calculates the total payable amount for the group.
+        check_bands() -> Self:
+            Validates the relationships and constraints among the group's bands.
+
+        get_payable(amount: int) -> int:
+            Calculates the total payable amount for the group based on the input value.
+
+        convert(rate: float):
+            Adjusts all bands in the group by the given rate.
     """
 
     bands: List[Band]
@@ -118,32 +124,29 @@ class BandsGroup(BaseModel):
     @model_validator(mode="after")
     def check_bands(self) -> Self:
         """
-        Validates the consistency of the bands in the group.
+        Validates the consistency of the bands within the group.
 
         Ensures:
-        - Allowance is non-negative.
-        - The first band's floor starts at 0.
+        - `allowance` is non-negative.
+        - The first band's floor is 0.
         - Each band's floor matches the ceiling of the previous band.
 
         Raises:
-            ValueError: If any validation fails.
+            ValueError: If any of these validations fail.
 
         Returns:
-            Self: The validated BandsGroup instance.
+            Self: The validated `BandsGroup` instance.
         """
         error_msg = []
-        if not isinstance(self.allowance, AllowanceFunction):
-            if self.allowance < 0:
-                error_msg.append("Allowance must be > 0 or class that implements the AllowanceFunction interface.")
-        b = self.bands
-        if b[0].floor != 0:
-            error_msg.append("Floor of first band must == 0")
-        for i in range(1, len(b)):  # check bands except first
-            if b[i].floor != b[i - 1].ceiling:  # band floor must == ceiling of previous band
-                error_msg.append(f"band floor [{b[i].floor}] does not match ceiling of previous band.")
-        if len(error_msg) > 0:
-            error_msg_joined = "\n" + "\n".join(error_msg)
-            raise ValueError(f"{error_msg_joined}")
+        if not isinstance(self.allowance, AllowanceFunction) and self.allowance < 0:
+            error_msg.append("Allowance must be non-negative or implement AllowanceFunction.")
+        if self.bands[0].floor != 0:
+            error_msg.append("The floor of the first band must be 0.")
+        for i in range(1, len(self.bands)):
+            if self.bands[i].floor != self.bands[i - 1].ceiling:
+                error_msg.append(f"Band {i} floor does not match the ceiling of the previous band.")
+        if error_msg:
+            raise ValueError("\n".join(error_msg))
         return self
 
     def get_payable(self, amount: int) -> int:
@@ -154,72 +157,117 @@ class BandsGroup(BaseModel):
             amount (int): The amount to calculate the payable value for.
 
         Returns:
-            int: The total payable amount across all bands.
+            int: The total payable amount across all bands, adjusted for the allowance.
         """
-        def _get_allowance(_amount: int) -> int:
-            if isinstance(self.allowance, AllowanceFunction):
-                return self.allowance.function(taxable=_amount)
-            else:
-                return self.allowance
-        return int(sum([b.get_payable(amount - _get_allowance(amount)) for b in self.bands]))
+        allowance = self.allowance.function(amount) if isinstance(self.allowance, AllowanceFunction) else self.allowance
+        return sum(b.get_payable(amount - allowance) for b in self.bands)
 
     def convert(self, rate: float):
-        [band.convert(rate) for band in self.bands]
+        """
+        Adjusts all bands in the group by the given rate.
+
+        Args:
+            rate (float): The conversion rate to scale the group's attributes.
+        """
+        for band in self.bands:
+            band.convert(rate)
+
 
 class TaxModel(BaseModel):
     """
     Represents a tax model consisting of multiple bands groups.
 
     Attributes:
-        tax_rules (List[BandsGroup]): A list of BandsGroup instances defining the rules.
+        tax_rules (List[BandsGroup]): A list of `BandsGroup` instances defining the rules.
         year (int): The tax year. Defaults to 2025.
-        non_sequential (bool): apply rules independently instead of sequentially
+        name (str): The name of the tax model. Defaults to "TaxModel".
+        non_sequential (bool): If True, applies each band's rules independently.
 
     Methods:
-        get_payable: Calculates the total tax payable based on the provided amount.
+        results(amount: int) -> Dict:
+            Calculates the detailed tax results, including payable amounts, total, and effective rate.
+
+        marginal_rate(amount: int, delta: int = 100) -> float:
+            Calculates the marginal tax rate for a given amount and delta.
+
+        sample(taxable_array: np.array) -> pd.DataFrame:
+            Generates a DataFrame summarizing tax calculations for a range of amounts.
+
+        convert(rate: float):
+            Adjusts all groups in the tax model by a given rate.
     """
     tax_rules: List[BandsGroup]
-    year: int=2025
-    name: str="TaxModel"
-    non_sequential: bool=False  # apply rules independently instead of sequentially
+    year: int = 2025
+    name: str = "TaxModel"
+    non_sequential: bool = False
 
     def results(self, amount: int) -> Dict:
-        dict = {}
+        """
+        Calculates the detailed tax results for a given amount.
+
+        Args:
+            amount (int): The taxable amount.
+
+        Returns:
+            Dict: A dictionary containing individual payable amounts, total payable, take-home amount, and effective rate.
+        """
+        result = {}
         taxable = amount
         total = 0
-        for r in self.tax_rules:
-            payable = r.get_payable(amount=taxable)
-            dict[r.name] = payable
+        for rule in self.tax_rules:
+            payable = rule.get_payable(taxable)
+            result[rule.name] = payable
             if not self.non_sequential:
                 taxable -= payable
             total += payable
-        dict["total payable"] = total
-        dict["take home"] = amount - total
-        dict["effective rate"] = total / amount
-        return dict
+        result.update({
+            "total payable": total,
+            "take home": amount - total,
+            "effective rate": total / amount if amount > 0 else 0,
+        })
+        return result
 
-    def marginal_rate(self, amount: int, delta: int=100) -> float:
-        r1 = self.results(amount=amount)["total payable"]
-        r2 = self.results(amount=amount + delta)["total payable"]
+    def marginal_rate(self, amount: int, delta: int = 100) -> float:
+        """
+        Calculates the marginal tax rate for a given amount and delta.
+
+        Args:
+            amount (int): The base taxable amount.
+            delta (int): The incremental change in the taxable amount. Defaults to 100.
+
+        Returns:
+            float: The marginal tax rate.
+        """
+        r1 = self.results(amount)["total payable"]
+        r2 = self.results(amount + delta)["total payable"]
         return (r2 - r1) / delta
 
-
-    def df_cols(self) -> List:
-        cols = list(self.sample(taxable_array=[10]).keys())
-        return cols
-
     def sample(self, taxable_array: np.array) -> pd.DataFrame:
-        cols = (list(self.results(amount=1).keys()))
-        process = np.vectorize(lambda amount:
-                               self.results(amount).values()
-                               )
-        raw_results = list(process(taxable_array))
-        df = pd.DataFrame(data=raw_results, index=taxable_array, columns=cols)
+        """
+        Generates a DataFrame summarizing tax calculations for a range of amounts.
+
+        Args:
+            taxable_array (np.array): Array of taxable amounts to evaluate.
+
+        Returns:
+            pd.DataFrame: A DataFrame with calculated results for each taxable amount,
+                          including marginal rates.
+        """
+        cols = list(self.results(1).keys())
+        results = [self.results(amount).values() for amount in taxable_array]
+        df = pd.DataFrame(results, index=taxable_array, columns=cols)
+
+        # Calculate marginal rates for each taxable amount
         marg_rate_fn = np.vectorize(lambda amount: self.marginal_rate(amount))
-        marg_rates = marg_rate_fn(taxable_array)
-        df["marginal rate"] = marg_rates
+        marginal_rates = marg_rate_fn(taxable_array)
+        df["marginal rate"] = marginal_rates
+
         return df
 
     def convert(self, rate: float):
         [rule.convert(rate) for rule in self.tax_rules]
 
+
+    def df_cols(self) -> List:
+        cols = list(self.sample(taxable_array=[10]).keys())
+        return cols
