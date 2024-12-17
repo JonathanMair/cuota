9554b8fad755297f1ee37a9304ca9d93bbd9c0e7
Jonathan Mair <jonathan.mair@gmail.com>
2024-12-17 11:45:45 +0100

    aboutn to upgrade to python 3.13

--- streamlit_tax.py
+++ streamlit_tax.py
@@ -3,37 +3,32 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from typing import Tuple, Dict
 
-from cuota.logic.calculations import Calculator
+from cuota.data_classes.spanish_tax_rules import SpanishAutonomoModel, SpanishRegimenGeneralModel
+from cuota.data_classes.foreign_tax_rules import UkEmployeeTaxModel, UkSelfEmployedTaxModel
+
+
+years = [2022, 2021, 2022, 2023]
 
 # @st.cache_data
-def get_cached_data() -> Tuple[pd.DataFrame, Dict]:
-    calculator = Calculator()
-    metrics = calculator.metrics
-    return calculator.data, metrics
+def get_cached_data():
+    aut_by_year = {year: SpanishAutonomoModel(year) for year in years}
+    gen_by_year = {year: SpanishRegimenGeneralModel(year) for year in years}
+    uk_employee = UkSelfEmployedTaxModel()
+    uk_self_empl = UkSelfEmployedTaxModel()
+    return aut_by_year, gen_by_year, uk_employee, uk_self_empl
 
-df, metrics = get_cached_data()
+
+aut_by_year, gen_by_year, uk_employee, uk_self_empl = get_cached_data()
 
 st.title("Tax App")
-st.subheader("Autónomo Net Income, Allowance = 5500€")
-# a = ("Social Security", metrics["NET_THIS"])
-# b = ("Income Tax", metrics["NET_THIS"])
-#
+
 plt.figure(figsize=(25, 25))
-# plt.stackplot(
-#     df.index,df[b], df[a], df.index,
-#     labels=["Net of Income Tax (Take-Home Pay)", "Net of Social Security", "Gross"]
-# )
-# plt.plot(df.index, df.index)
-#
-# # plt.title('Net Income After Application of Tax Rules')
-# plt.xlabel('Gross Income')
-# # plt.ylabel('Net Income')
+for year in years:
 
 
 
 def plot_chart():
     metric = st.session_state.metric
-    print(metric)
     cols = [col for col in df.columns if metric in col]
     for col in cols:
         plt.plot(df.index, df[col], label=col[0])
@@ -42,7 +37,3 @@ def plot_chart():
     plt.grid(True)
     st.pyplot(plt)
 
-
-st.selectbox(label="Metric", options=metrics.values(), index=0, key="metric", on_change=plot_chart)
-
-st.write(df.head(-20))
